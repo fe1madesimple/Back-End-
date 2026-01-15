ff5df799df84fb896aac637552c1894c86dca842
viktor ajayi <victorajayidamilare@gmail.com>
2026-01-15 07:18:15 +0100

    userProfileSchema added

---
+++ src/modules/user/validators/user.validator.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const updateProfileSchema = z.object({
+  body: z.object({
+    firstName: z.string().min(2).optional(),
+    lastName: z.string().min(2).optional(),
+    profileColor: z
+      .string()
+      .regex(/^#[0-9A-F]{6}$/i, 'Invalid hex color')
+      .optional(),
+    targetExamDate: z.string().datetime().optional(),
+    dailyStudyGoal: z.number().min(1).max(24).optional(),
+    focusSubjects: z.array(z.string()).optional(),
+  }),
+});
