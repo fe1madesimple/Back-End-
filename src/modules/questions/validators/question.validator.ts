90f41c19c9d6a73dec0e232443b130b909144e4a
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-02 21:21:24 +0100

    submit essay question schema

--- src/modules/questions/validators/question.validator.ts
+++ src/modules/questions/validators/question.validator.ts
@@ -1,9 +1,15 @@
-import z from "zod";
-
+import z from 'zod';
 
 export const attemptQuestionSchema = z.object({
   body: z.object({
     answer: z.string().min(1),
+    timeTakenSeconds: z.number().int().min(0).optional(),
+  }),
+});
+
+export const submitEssaySchema = z.object({
+  body: z.object({
+    answer: z.string().min(50, 'Essay must be at least 50 characters'),
     timeTakenSeconds: z.number().int().min(0).optional(),
   }),
 });
