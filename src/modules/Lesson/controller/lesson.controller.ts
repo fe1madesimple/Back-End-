5e278a69ccc955dad99d703489b458f2fa66d37f
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-02 15:25:54 +0100

    track time spent controller

--- src/modules/Lesson/controller/lesson.controller.ts
+++ src/modules/Lesson/controller/lesson.controller.ts
@@ -15,15 +15,28 @@ export const getLessonById = asyncHandler(async (req: Request, res: Response): P
   sendSuccess(res, 'Lesson retrieved successfully', { lesson });
 });
 
-export const trackVideoProgress = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+export const trackVideoProgress = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const userId = req.user!.user.id;
+    const { id } = req.params;
+    const { currentTime } = req.body;
+
+    if (!id) return new AppError('lesson id must be provided');
+
+    await lessonService.trackVideoProgress(userId, id, currentTime);
+
+    sendSuccess(res, 'Video progress tracked');
+  }
+);
+
+export const trackTimeSpent = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user!.user.id;
   const { id } = req.params;
-    const { currentTime } = req.body;
+    const { seconds } = req.body;
     
-    if (!id) return new AppError("lesson id must be provided")
-    
+    if (!id) throw new AppError("lesson id must be provided")
 
-  await lessonService.trackVideoProgress(userId, id, currentTime);
+  await lessonService.trackTimeSpent(userId, id, seconds);
 
-  sendSuccess(res, 'Video progress tracked');
+  sendSuccess(res, 'Time tracked successfully');
 });
