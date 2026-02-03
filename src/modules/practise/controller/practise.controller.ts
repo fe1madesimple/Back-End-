85fd21addc8f5250168f7f13124c65707166f45b
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-04 00:09:28 +0100

    get mixed practise questions controller

--- src/modules/practise/controller/practise.controller.ts
+++ src/modules/practise/controller/practise.controller.ts
@@ -19,11 +19,23 @@ export const getQuickQuiz = asyncHandler(async (req: Request, res: Response) => 
 // src/modules/content/controller/content.controller.ts
 
 export const getTopicChallenge = asyncHandler(async (req: Request, res: Response) => {
-    const { moduleId } = req.params;
-    
-    if (!moduleId) throw new AppError("module id needed")
+  const { moduleId } = req.params;
+
+  if (!moduleId) throw new AppError('module id needed');
 
   const challenge = await practiseService.getTopicChallenge(moduleId);
 
   sendSuccess(res, 'Topic challenge retrieved', challenge);
+});
+
+// src/modules/content/controller/content.controller.ts
+
+export const getMixedPractice = asyncHandler(async (req: Request, res: Response) => {
+  const { subjectId } = req.params;
+
+  if (!subjectId) throw new AppError('subject id must be supplied');
+
+  const practice = await practiseService.getMixedPractice(subjectId);
+
+  sendSuccess(res, 'Mixed practice retrieved', practice);
 });
