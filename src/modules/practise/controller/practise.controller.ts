c77f863c11db022e9337f4da4edb397caf1fd701
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-03 23:57:53 +0100

    get topic challenge controller added

--- src/modules/practise/controller/practise.controller.ts
+++ src/modules/practise/controller/practise.controller.ts
@@ -1,18 +1,29 @@
-import { AppError } from "@/shared/utils";
-import { Request, Response } from "express";
-import { asyncHandler } from "@/shared/utils";
-import practiseService from "../service/practise.service";
-import { sendSuccess } from "@/shared/utils";
-
+import { AppError } from '@/shared/utils';
+import { Request, Response } from 'express';
+import { asyncHandler } from '@/shared/utils';
+import practiseService from '../service/practise.service';
+import { sendSuccess } from '@/shared/utils';
 
 // src/modules/content/controller/content.controller.ts
 
 export const getQuickQuiz = asyncHandler(async (req: Request, res: Response) => {
-    const { moduleId } = req.params;
-    
-    if (!moduleId) throw new AppError("module id must be supplied")
+  const { moduleId } = req.params;
+
+  if (!moduleId) throw new AppError('module id must be supplied');
 
   const quiz = await practiseService.getQuickQuiz(moduleId);
 
   sendSuccess(res, 'Quick quiz retrieved', quiz);
+});
+
+// src/modules/content/controller/content.controller.ts
+
+export const getTopicChallenge = asyncHandler(async (req: Request, res: Response) => {
+    const { moduleId } = req.params;
+    
+    if (!moduleId) throw new AppError("module id needed")
+
+  const challenge = await practiseService.getTopicChallenge(moduleId);
+
+  sendSuccess(res, 'Topic challenge retrieved', challenge);
 });
