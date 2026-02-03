354ac405a7864ba407f981b04f4009f56a602b29
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-04 00:56:59 +0100

    past question Query schema

--- src/modules/practise/validators/practise.validators.ts
+++ src/modules/practise/validators/practise.validators.ts
@@ -1,9 +1,22 @@
 import z from 'zod';
 
-
-
 export const moduleIdParamSchema = z.object({
   params: z.object({
     moduleId: z.string().cuid(),
+  }),
+});
+
+// src/modules/content/validator/content.validator.ts
+
+export const pastQuestionsQuerySchema = z.object({
+  query: z.object({
+    subject: z.string().optional(),
+    year: z
+      .string()
+      .regex(/^\d{4}$/)
+      .optional(),
+    examType: z.string().optional(),
+    page: z.string().regex(/^\d+$/).optional(),
+    limit: z.string().regex(/^\d+$/).optional(),
   }),
 });
