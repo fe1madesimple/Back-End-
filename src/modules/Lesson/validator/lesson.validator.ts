c7a9e475c7691524357a352d6fe270c34b92947a
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-03 03:38:49 +0100

    lesson schema patch

--- src/modules/Lesson/validator/lesson.validator.ts
+++ src/modules/Lesson/validator/lesson.validator.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const trackVideoSchema = z.object({
   body: z.object({
     currentTime: z.number().min(0).int(),
+    videoDuration: z.number().int().optional(),
   }),
 });
 
