f65fccab26fd90803b0ffbcb2b2a931447ec6572
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-04 01:41:44 +0100

    get subject details controller

--- src/modules/progress/controllers/progress.controller.ts
+++ src/modules/progress/controllers/progress.controller.ts
@@ -1,7 +1,7 @@
 // src/modules/progress/controller/progress.controller.ts
 
 import { Request, Response } from 'express';
-import { asyncHandler } from '@/shared/utils';
+import { AppError, asyncHandler } from '@/shared/utils';
 import { sendSuccess } from '@/shared/utils/response';
 import progressService from '../services/progress.service';
 
@@ -11,4 +11,17 @@ export const getDashboardStats = asyncHandler(async (req: Request, res: Response
   const stats = await progressService.getDashboardStats(userId);
 
   sendSuccess(res, 'Dashboard stats retrieved', stats);
+});
+
+// src/modules/progress/controller/progress.controller.ts
+
+export const getSubjectProgressDetail = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+  const { subjectId } = req.params;
+
+  if (!subjectId) throw new AppError('subject id must be provided');
+
+  const progress = await progressService.getSubjectProgressDetail(userId, subjectId);
+
+  sendSuccess(res, 'Subject progress retrieved', progress);
 });
