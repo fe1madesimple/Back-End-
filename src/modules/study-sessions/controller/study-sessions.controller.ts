324eeadad49ecf6b49b8536623c50edfd73888c6
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-04 13:16:44 +0100

    study sessions controller

--- src/modules/study-sessions/controller/study-sessions.controller.ts
+++ src/modules/study-sessions/controller/study-sessions.controller.ts
@@ -3,13 +3,13 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '@/shared/utils';
 import { sendSuccess } from '@/shared/utils/response';
-
+import studySessionsService from '../service/study-sessions.service';
 
 export const startSession = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user!.user.id;
   const data = req.body;
 
-  const session = await studySessionService.startSession(userId, data);
+  const session = await studySessionsService.startSession(userId, data);
 
   sendSuccess(res, 'Study session started', session, 201);
 });
