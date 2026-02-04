f1d36f16663407c352a654765ee3f80db61d72b9
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-04 18:59:27 +0100

    podcast detail response interface

--- src/modules/podcasts/interface/podcast.interface.ts
+++ src/modules/podcasts/interface/podcast.interface.ts
@@ -14,3 +14,20 @@ export interface PodcastListResponse {
   total: number;
 }
 
+export interface PodcastDetailResponse {
+  podcast: {
+    id: string;
+    title: string;
+    description: string | null;
+    subject: string | null;
+    audioUrl: string;
+    thumbnail: string | null;
+    duration: number | null;
+    order: number;
+  };
+  progress: {
+    listenedSeconds: number;
+    isCompleted: boolean;
+    completedAt: Date | null;
+  } | null;
+}
