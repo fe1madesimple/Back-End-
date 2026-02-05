58b0ab5b307481ba29099752675e0777cee65afc
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-06 00:03:15 +0100

    caselaw validators added

---
+++ src/modules/caseLaw/validator/case.validator.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import { CaseJurisdiction, CaseFrequency } from '@prisma/client';
+
+export const searchCasesSchema = z.object({
+  query: z.object({
+    search: z.string().optional(),
+    subject: z.string().optional(),
+    jurisdiction: z.nativeEnum(CaseJurisdiction).optional(),
+    frequency: z.nativeEnum(CaseFrequency).optional(),
+    page: z
+      .string()
+      .optional()
+      .transform((val) => (val ? parseInt(val) : 1)),
+    limit: z
+      .string()
+      .optional()
+      .transform((val) => (val ? parseInt(val) : 10)),
+  }),
+});
