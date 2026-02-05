4ceab3aa25c44109171065bff22fc141a66b49ba
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-06 00:39:36 +0100

    get case details controller

--- src/modules/caseLaw/controller/case.controller.ts
+++ src/modules/caseLaw/controller/case.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { asyncHandler } from '@/shared/utils';
+import { AppError, asyncHandler } from '@/shared/utils';
 import { sendSuccess } from '@/shared/utils/response';
 import caseService from '../service/case.service';
 
@@ -10,4 +10,17 @@ export const searchCases = asyncHandler(async (req: Request, res: Response) => {
   const result = await caseService.searchCases(userId, query);
 
   sendSuccess(res, 'Cases retrieved successfully', result);
+});
+
+export const getCaseDetails = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+    const { id } = req.params;
+    
+    if (!id) throw new AppError("case id must be provided"
+        
+    )
+
+  const caseDetails = await caseService.getCaseDetails(userId, id);
+
+  sendSuccess(res, 'Case details retrieved successfully', caseDetails);
 });
