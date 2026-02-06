8bbf791695b1985ee5c9d4d39b9b749ae2e28259
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-06 01:15:37 +0100

    create playlist controller

---
+++ src/modules/playLists/controller/playlists.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { asyncHandler } from '@/shared/utils';
+import { sendSuccess } from '@/shared/utils/response';
+import playlistsService from '../service/playlists.service';
+
+
+
+
+export const createPlaylist = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+  const data = req.body;
+
+  const playlist = await playlistsService.createPlaylist(userId, data);
+
+  sendSuccess(res, 'Playlist created successfully', playlist, 201);
+});
