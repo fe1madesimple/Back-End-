9cd006347d36381cd84374a70dafdae33e3e2c48
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-06 01:36:47 +0100

    get user playlists

--- src/modules/playLists/controller/playlists.controller.ts
+++ src/modules/playLists/controller/playlists.controller.ts
@@ -3,9 +3,6 @@ import { asyncHandler } from '@/shared/utils';
 import { sendSuccess } from '@/shared/utils/response';
 import playlistsService from '../service/playlists.service';
 
-
-
-
 export const createPlaylist = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user!.user.id;
   const data = req.body;
@@ -13,4 +10,12 @@ export const createPlaylist = asyncHandler(async (req: Request, res: Response) =
   const playlist = await playlistsService.createPlaylist(userId, data);
 
   sendSuccess(res, 'Playlist created successfully', playlist, 201);
+});
+
+export const getUserPlaylists = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+
+  const result = await playlistsService.getUserPlaylists(userId);
+
+  sendSuccess(res, 'Playlists retrieved successfully', result);
 });
