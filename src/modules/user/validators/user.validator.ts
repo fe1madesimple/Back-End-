688d360f1571bac516c88bfc2a227bd3ca1e1d08
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-07 17:59:26 +0100

    profile schema patch

--- src/modules/user/validators/user.validator.ts
+++ src/modules/user/validators/user.validator.ts
@@ -2,8 +2,7 @@ import { z } from 'zod';
 
 export const updateProfileSchema = z.object({
   body: z.object({
-    firstName: z.string().min(2).optional(),
-    lastName: z.string().min(2).optional(),
+    fullName: z.string().min(2).optional(),
     profileColor: z
       .string()
       .regex(/^#[0-9A-F]{6}$/i, 'Invalid hex color')
