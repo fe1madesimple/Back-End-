b201b9481d413efd24a7e021a498a7d6a2fce692
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-12 23:34:02 +0100

    end session controller

--- src/modules/study-sessions/controller/study-sessions.controller.ts
+++ src/modules/study-sessions/controller/study-sessions.controller.ts
@@ -6,12 +6,21 @@ import { sendSuccess } from '@/shared/utils/response';
 import studySessionsService from '../service/study-sessions.service';
 import { AppError } from '@/shared/utils';
 
-
-
 export const startSession = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user!.user.id;
 
   const session = await studySessionsService.startSession(userId);
 
   sendSuccess(res, 'Study session started', session, 201);
+});
+
+export const endSession = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+  const { sessionId } = req.params;
+
+  if (!sessionId) throw new AppError('session id must be present');
+
+  await studySessionsService.endSession(userId, sessionId);
+
+  sendSuccess(res, 'Study session ended');
 });
