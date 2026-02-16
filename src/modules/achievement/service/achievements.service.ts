5078383d13c2e1cb99952f59ea2eb1225797c541
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-16 13:11:00 +0100

    get all achievements

--- src/modules/achievement/service/achievements.service.ts
+++ src/modules/achievement/service/achievements.service.ts
@@ -1,9 +1,13 @@
 import { prisma } from '@/shared/config';
 
-
-
 class AchievementService {
+  private unlockedCache = new Map<string, Set<string>>();
 
+  async getAllAchievements() {
+    return await prisma.achievement.findMany({
+      orderBy: [{ type: 'asc' }, { createdAt: 'asc' }],
+    });
+  }
 }
 
-export default new AchievementService()
+export default new AchievementService();
