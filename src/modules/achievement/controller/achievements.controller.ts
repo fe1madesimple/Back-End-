8a7f183d1cb4d8f9386d7d88aec331e117a4b484
viktor ajayi <victorajayidamilare@gmail.com>
2026-02-17 17:40:57 +0100

    achievmens conrtoller

---
+++ src/modules/achievement/controller/achievements.controller.ts
@@ -0,0 +1,17 @@
+// src/modules/achievements/controllers/achievement.controller.ts
+
+import { Request, Response } from 'express';
+import { asyncHandler } from '@/shared/utils';
+import { sendSuccess } from '@/shared/utils';
+import achievementsService from '../service/achievements.service';
+
+export const getAllAchievements = asyncHandler(async (req: Request, res: Response) => {
+  const achievements = await achievementsService.getAllAchievements();
+  sendSuccess(res, 'Achievements retrieved successfully', achievements);
+});
+
+export const getUserAchievements = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.user.id;
+  const achievements = await achievementsService.getUserAchievements(userId);
+  sendSuccess(res, 'User achievements retrieved successfully', achievements);
+});
